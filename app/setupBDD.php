d2b1c1ec26b730ad5b2e566f66034d9902a11847
Tenebrosful <hugobernard39@gmail.com>
2020-09-24 10:19:57 +0200

    Ajout du display error

--- app/setupBDD.php
+++ app/setupBDD.php
@@ -4,6 +4,10 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $capsule = new Capsule;
 
 
@@ -11,7 +15,7 @@ if (is_array($bddConfig = parse_ini_file('../config/bdd.ini')))
     $capsule->addConnection($bddConfig);
 else {
     echo "ERREUR : Fichier de configuration de la base de donnée introuvable";
-    exit(); 
+    exit();
 }
 
 //Make this Capsule instance available globally.
