96c56f9de28093c972c33ca84dd4f44e50980472
David Baraniak <engenteur@davidbaraniak.fr>
2020-09-29 09:19:10 +0200

    Tries to create relationships for Localisation

--- src/Domain/Localisation/Localisation.php
+++ src/Domain/Localisation/Localisation.php
@@ -1,23 +1,21 @@
 <?php
 
-
 namespace App\Domain\Localisation;
 
+// Localisation est un modèle d'Eloquent
 use Illuminate\Database\Eloquent\Model;
+// On importe les entités dont on a besoin dans le code
 use App\Domain\Utilisateur\Utilisateur;
 
-class Localisation extends Model
-{
+class Localisation extends Model {
     /*
      * Nom de la table
      */
     protected $table = 'localisations';
-
     /*
      * Nom de la primary key
      */
     protected $primaryKey = 'id';
-
     /*
      * Liste des colones modifiables
      *
@@ -27,17 +25,22 @@ class Localisation extends Model
         'latitude',
         'longitude'
     ];
-
     /*
-     * Liste des colones à caché en cas de conversion en String / JSON
+     * Liste des colones à cacher en cas de conversion en String / JSON
      *
      * @var array
      */
     protected $hidden = [];
-
+    /*
+     * Une méthode pour retrouver l'utilisateur d'une localisation
+     */
     public function utilisateur() {
-        // Un seul utilisateur pour chaque localisation
-        return $this->hasOne('App\Domain\Utilisateur');
+        // One to one correct
+        // Localisation belongs to Utilisateur (si 2 personnes peuvent être au même endroit)
+        return $this->belongsTo('App\Domain\Utilisateur\Utilisateur');
+        //return $this->belongsToMany('App\Domain\Utilisateur\Utilisateur', 'utilisateurlocalisation', 'id_user', 'id_localisation');
+        //return $this->belongsToMany('App\Domain\Utilisateur\Utilisateur')->using('App\Domain\UtilisateurLocalisation');
+        //return $this->hasOneThrough('App\Domain\Utilisateur\Utilisateur', 'App\Domain\UtilisateurLocalisation\UtilisateurLocalisation', 'id_localisation', 'id_user');
     }
 
 }
