0fe475238bb8930bb474dd0d907eea59555b918a
Tenebrosful <hugobernard39@gmail.com>
2020-10-01 12:08:45 +0200

    Correction LoggedMiddleware.php

--- src/Application/Middleware/LoggedMiddleware.php
+++ src/Application/Middleware/LoggedMiddleware.php
@@ -3,8 +3,6 @@
 
 namespace App\Application\Middleware;
 
-
-use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface as Middleware;
@@ -14,21 +12,6 @@ use Slim\Routing\RouteContext;
 class LoggedMiddleware implements Middleware
 {
 
-    /**
-     * @var ResponseFactoryInterface
-     */
-    private ResponseFactoryInterface $responseFactory;
-
-    /**
-     * Constructor.
-     *
-     * @param ResponseFactoryInterface $responseFactory The response factory
-     */
-    public function __construct(ResponseFactoryInterface $responseFactory)
-    {
-        $this->responseFactory = $responseFactory;
-    }
-
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         if (!empty($_SESSION['user_id'])) {
@@ -37,7 +20,7 @@ class LoggedMiddleware implements Middleware
             $routeParser = RouteContext::fromRequest($request)->getRouteParser();
             $url = $routeParser->urlFor('signin', ['message' => "L'accès à cette page require d'être connecté !"]);
 
-            return $this->responseFactory->createResponse()->withHeader('Location', $url)->withStatus(302);
+            return $handler->handle($request)->withHeader('Location', $url)->withStatus(302);
         }
     }
 }
