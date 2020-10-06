f15cca114ba311a90c2be70582f81998b1d56df4
Tenebrosful <hugobernard39@gmail.com>
2020-10-06 09:54:57 +0200

    Ajout de getByEmail

--- src/Domain/Utilisateur/Utilisateur.php
+++ src/Domain/Utilisateur/Utilisateur.php
@@ -5,6 +5,9 @@ namespace App\Domain\Utilisateur;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static where(string $colonne, string $comparateur, mixed $valeur)
+ */
 class Utilisateur extends Model
 {
     /*
@@ -53,7 +56,16 @@ class Utilisateur extends Model
         return $this->belongsToMany("App\Domain\Message\Message", "messagerie", "id_user_destinataire", "id_message")->get();
     }
 
-    public function localisations() {
+    public function localisations()
+    {
         return $this->belongsToMany('App\Domain\Localisation\Localisation', 'utilisateurlocalisation', 'id_user', 'id_localisation');
+    }
+
+    /*
+     * @param string email
+     */
+    static public function getByEmail($email)
+    {
+        return Utilisateur::where('email', '=', $email)->first();
     }
 }
