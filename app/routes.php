5e62240d2d3642561393140812af03b48ccf5593
Tenebrosful <hugobernard39@gmail.com>
2020-10-06 11:46:52 +0200

    Suppression d'import useless

--- app/routes.php
+++ app/routes.php
@@ -2,9 +2,6 @@
 declare(strict_types=1);
 
 use App\Application\Actions\AuthenticateAction;
-use App\Application\Actions\Messages\MessageReadAction;
-use App\Application\Actions\User\ListUsersAction;
-use App\Application\Actions\User\ViewUserAction;
 use App\Application\Middleware\LoggedMiddleware;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
