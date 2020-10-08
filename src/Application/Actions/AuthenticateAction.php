cabdb6c9876009904cdf8f192dbdddb804a49851
Hugo BERNARD 'Tenebrosful <hugobernard39@gmail.com>
2020-10-08 04:03:22 +0200

    Mise à jour nom de variable + update bootstrap

--- src/Application/Actions/AuthenticateAction.php
+++ src/Application/Actions/AuthenticateAction.php
@@ -6,6 +6,7 @@ use App\Domain\Utilisateur\Utilisateur;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Slim\Routing\RouteContext;
 
 /**
  * Action
@@ -27,25 +28,27 @@ final class AuthenticateAction
         array $args = []
     ): ResponseInterface
     {
-        if (isset($_POST['username']) && $_POST['username'] != '' && isset($_POST['password']) && $_POST['password'] != '') {
-            $username = htmlentities($_POST['username']);
+        if (!empty($_POST['email']) && !empty($_POST['password'])) {
+            $email = htmlentities($_POST['email']);
             $password = htmlentities($_POST['password']);
+
+            $routeParser = RouteContext::fromRequest($request)->getRouteParser();
+
             try {
-                $donneesUtilisateur = Utilisateur::getByEmail($username);
+                $donneesUtilisateur = Utilisateur::getByEmail($email);
             } catch (ModelNotFoundException $e) {
-                $_SESSION['message'] = "La combinaison d'authentification est incorrecte !";
-                return $response->withHeader('Location', 'signin')->withStatus(301);
+                $_SESSION['message'] = "La combinaison email/mot de passe est incorrect !";
+
+                return $response->withHeader('Location', $routeParser->urlFor('signin'))->withStatus(301);
             }
             if (password_verify($password, $donneesUtilisateur->mdpCrypte)) {
                 $_SESSION['user_id'] = $donneesUtilisateur->id;
-                $_SESSION['username'] = $donneesUtilisateur->email;
-                /*$routeParser = RouteContext::fromRequest($request)->getRouteParser();
-                $url = $routeParser->urlFor('welcome');
-                return $response->withHeader('Location', $url)->withStatus(301);*/
-                return $response->withHeader('Location', '/welcome')->withStatus(301);
+                $_SESSION['email'] = $donneesUtilisateur->email;
+
+                return $response->withHeader('Location', $routeParser->urlFor('welcome'))->withStatus(301);
             } else {
-                $_SESSION['message'] = "La combinaison d'authentification est incorrecte !";
-                return $response->withHeader('Location', 'signin')->withStatus(301);
+                $_SESSION['message'] = "La combinaison email/mot de passe est incorrect !";
+                return $response->withHeader('Location', $routeParser->urlFor('signin'))->withStatus(301);
             }
         }
     }
