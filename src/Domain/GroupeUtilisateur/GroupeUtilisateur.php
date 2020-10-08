6e303b1de5c1bbfff1c96ab3e52de0f22977c5b5
Tenebrosful <hugobernard39@gmail.com>
2020-10-08 09:03:01 +0200

    Ajout GroupeUtilisateur.php

---
+++ src/Domain/GroupeUtilisateur/GroupeUtilisateur.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace App\Domain\GroupeUtilisateur;
+
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class GroupeUtilisateur extends Pivot
+{
+    /*
+     * Nom de la table
+     */
+    protected $table = 'utilisateurlocalisation';
+
+    /*
+     * Nom de la primary key
+     */
+    protected $primaryKey = [
+        'id_user',
+        'id_localisation'
+    ];
+
+    protected $incrementing = false;
+
+    /*
+     * Liste des colones modifiables
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id_groupe',
+        'id_user'
+    ];
+
+    /*
+     * Liste des colones à caché en cas de conversion en String / JSON
+     *
+     * @var array
+     */
+    protected $hidden = [];
+}
